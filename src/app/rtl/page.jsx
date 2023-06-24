1a2073bc9af592117720e1426c78db740857e951
Ali <120955025+AliNikseresht@users.noreply.github.com>
2023-06-24 23:18:15 +0330

    Delete src directory

--- src/app/rtl/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RTL = () => {
-  return (
-    <div className='min-h-[100vh] max-w-[1212px] flex flex-col m-auto xl:ml-[20%] bg-gray-100'>Cooming Soon...</div>
-  )
-}
-
-export default RTL
